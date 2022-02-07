4da2c3bf3df66439fa2223985666e4398d38657c
nitishchaudhary <006nschaudhary006@gmail.com>
2022-02-07 13:12:58 +0530

    commit

--- Codeshare/settings.py
+++ Codeshare/settings.py
@@ -152,7 +152,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR,'assests')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 django_heroku.settings(locals())
