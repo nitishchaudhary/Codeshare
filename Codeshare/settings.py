54da85ecb99d1125a53aff319797e4a68ca1ae09
Nitish <55824689+nitishchaudhary@users.noreply.github.com>
2022-02-07 19:20:19 +0530

    Update settings.py

--- Codeshare/settings.py
+++ Codeshare/settings.py
@@ -152,8 +152,8 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS =[
 #     os.path.join(BASE_DIR,'static')
 # ]
-WHITENOISE_MANIFEST_STRICT = False
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# WHITENOISE_MANIFEST_STRICT = False
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
