1bce23c6c617e59d5b53a6ce41ac6c5e5509be42
Nitish <55824689+nitishchaudhary@users.noreply.github.com>
2022-02-07 19:16:00 +0530

    Update settings.py

--- Codeshare/settings.py
+++ Codeshare/settings.py
@@ -152,6 +152,7 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS =[
 #     os.path.join(BASE_DIR,'static')
 # ]
+WHITENOISE_MANIFEST_STRICT = False
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
