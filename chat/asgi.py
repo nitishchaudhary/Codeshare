4355230572d8fe7d3507818ebd22d6dedb5b5ab3
Nitish <55824689+nitishchaudhary@users.noreply.github.com>
2022-02-08 12:18:10 +0530

    Update asgi.py

--- chat/asgi.py
+++ chat/asgi.py
@@ -2,6 +2,7 @@ import os
 import chat.routing
 from channels.routing import ProtocolTypeRouter,URLRouter
 from django.core.asgi import get_asgi_application
+# from channels.routing import get_default_application
 from channels.auth import AuthMiddlewareStack
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Codeshare.settings')
 
@@ -13,3 +14,4 @@ application = ProtocolTypeRouter({
           )
      )
 })
+
